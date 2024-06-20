6e9f2ae07e830cc58dcddde7a79b1a595fd507b7
Santiago :) <santiagoespinosagiraldo1@gmail.com>
2024-06-20 12:48:22 -0500

    fs

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
+const fs = require('fs');
 
-console.log('hola');
-console.warn('ocurrio un error ...');
-console.error(new Error('ocurrio un error ...'));
+fs.readFile('indexA.html', 'utf-8', (err, contenido) => {
+    if(err){
+        console.log(err);
+    }else{
+        console.log(contenido);
+    }
+
+    console.log('mensaje...')
+});
+
+
+fs.rename('index.html','main.html',(err) =>{
+
+    if(err){
+        throw(err);
+    }else{
+        console.log('Archivo renombrado');
+    }
+
+});
+
+
+// add content to file at the end of the file
+
+
+fs.appendFile('index.html','<p>Hola</p>',(err) =>{
+    if(err){
+        throw(err);
+    }else{
+        console.log('Contenido agregado');
+    }
+
+
+});
+
+
+
+fs.writeFile('index.html','cont 1',(err) =>{
+    if(err){
+        throw(err);
+    }else{
+        console.log('Contenido agregado');
+    }
+});
+
+
+fs.unlink('index copy.html',(err) =>{
+    if(err){
+        throw(err);
+    }else{
+        console.log('Archivo eliminado');
+    }
+});
+
+
 
