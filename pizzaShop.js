8a96173acb394f689f57aae9052c38f8285e0c12
Santiago :) <santiagoespinosagiraldo1@gmail.com>
2024-06-21 11:44:38 -0500

    another example for events

---
+++ pizzaShop.js
@@ -0,0 +1,25 @@
+const orderStatus = () => {
+    return Math.random() < 0.8;
+};
+
+const myPizzaOrder = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(orderStatus()){
+            resolve('Your pizza is on the way');
+        }else{
+            reject('Your pizza is not available');
+        }
+    
+    
+    }, 1000);
+});
+
+const manageOrderSuccess = (orderConfirmed) => {
+    console.log(orderConfirmed);
+}
+
+const manageOrderFailure = (orderDeclined) => {
+    console.log(orderDeclined);
+}
+
+myPizzaOrder.then(manageOrderSuccess, manageOrderFailure);
