7ed86328245ab3dea834342bd77f94423be9ad50
Santiago :) <santiagoespinosagiraldo1@gmail.com>
2024-06-25 14:00:58 -0500

    adding courses

---
+++ routing/courses.js
@@ -0,0 +1,45 @@
+let infoCourses = {
+    'programming':[
+        {
+            id: 1,
+            title: 'Python Programming',
+            language: 'Python',
+            level: 'Beginner',
+            views: 1000
+        },
+        {
+            id: 2,
+            title: 'Java Programming',
+            language: 'Java',
+            level: 'Intermediate',
+            views: 500
+        },
+        {
+            id: 3,
+            title: 'C# Programming',
+            language: 'C#',
+            level: 'Advanced',
+            views: 200
+            
+        }
+    ],
+    'Maths':[
+        {
+            id: 1,
+            title: 'Learn calculus',
+            theme: 'Calculus',
+            views: 1232,
+            level: 'Beginner'
+        },
+        {
+            id:2,
+            title: 'Learn algebra',
+            theme: 'Algebra',
+            views: 1232,
+            level: 'Intermediate'
+        }
+    ]
+
+}
+
+module.exports.infoCourses = infoCourses;
