4e85035b22052f7730ff09e0552efb8496392bb4
Santiago :) <santiagoespinosagiraldo1@gmail.com>
2024-06-27 00:07:41 -0500

    express

--- express/data/courses.js
+++ express/data/courses.js
@@ -4,21 +4,21 @@ let infoCourses = {
             id: 1,
             title: 'Python Programming',
             language: 'python',
-            level: 'Beginner',
+            level: 'beginner',
             views: 1000
         },
         {
             id: 2,
             title: 'Java Programming',
             language: 'java',
-            level: 'Intermediate',
+            level: 'intermediate',
             views: 500
         },
         {
             id: 3,
             title: 'C# Programming',
             language: 'c#',
-            level: 'Advanced',
+            level: 'advanced',
             views: 200
             
         }
@@ -29,14 +29,14 @@ let infoCourses = {
             title: 'Learn calculus',
             theme: 'calculus',
             views: 1232,
-            level: 'Beginner'
+            level: 'beginner'
         },
         {
             id:2,
             title: 'Learn algebra',
             theme: 'algebra',
             views: 1232,
-            level: 'Intermediate'
+            level: 'intermediate'
         }
     ]
 
--- express/routers/programming.js
+++ express/routers/programming.js
@@ -4,6 +4,8 @@ const {programming} = require('../data/courses').infoCourses;
 
 const routerProgramming = express.Router();
 
+// Middleware
+routerProgramming.use(express.json());
 
 
 routerProgramming.get('/', (req, res) => {
@@ -21,5 +23,62 @@ routerProgramming.get('/:language', (req, res) => {
 
     res.send(JSON.stringify(results));
 });
+
+
+routerProgramming.post('/', (req, res) => {
+    let newCourse = req.body;
+
+    console.log('Received new course data:', newCourse);
+
+    if (!newCourse.id || !newCourse.title || !newCourse.language || !newCourse.level || !newCourse.views) {
+        return res.status(400).send('Invalid course data');
+    }
+
+    programming.push(newCourse);
+    res.send(programming);
+});
+
+
+
+routerProgramming.put('/:id', (req, res) => {
+    const updatedCourse = req.body;
+    const id = req.params.id;
+
+    const index = programming.findIndex(course => course.id == id); // == because id is a string
+
+    if (index => 0){
+        programming[index] = updatedCourse;
+    }
+    res.send(JSON.stringify(programming));
+
+});
+
+
+routerProgramming.patch('/:id', (req, res) => {
+    const updatedCourse = req.body;
+    const id = req.params.id;
+
+    const index = programming.findIndex(course => course.id == id); // == because id is a string
+
+    if (index => 0){
+        const courseToModify = programming[index];
+        Object.assign(courseToModify, updatedCourse);
+    }
+    res.send(JSON.stringify(programming));
+
+});
+
+routerProgramming.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    const index = programming.findIndex(course => course.id == id); // == because id is a string
+
+    if (index => 0){
+        programming.splice(index, 1);
+    }
+    res.send(JSON.stringify(programming));
+
+});
+
 
 module.exports = routerProgramming;
